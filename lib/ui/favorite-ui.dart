6c307a60cf01a66c50593027e7a11fd0f134b48d
Anthony Ramos Romero <anthonyramosdev@gmail.com>
2021-06-19 03:53:03 -0500

    Avance ....

--- lib/ui/favorite-ui.dart
+++ lib/ui/favorite-ui.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:practica04/widget/item-header-custom.dart';
 import 'package:practica04/widget/item-list-custom.dart';
 
 class FavoriteUI extends StatelessWidget{
@@ -8,6 +9,17 @@ class FavoriteUI extends StatelessWidget{
 
     return Scaffold(
       body: UI(),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.black26,
+
+        items: [
+          BottomNavigationBarItem(icon: Icon( Icons.ballot_sharp )      , label: ""       ),
+          BottomNavigationBarItem(icon: Icon( Icons.add_sharp )         , label: ""       ),
+          BottomNavigationBarItem(icon: Icon( Icons.favorite )              , label: ""       ),
+          BottomNavigationBarItem(icon: Icon( Icons.ac_unit_sharp )     , label: ""       ),
+          BottomNavigationBarItem(icon: Icon( Icons.add_business_sharp ), label: ""       ),
+        ],
+      ),
     );
 
   }
@@ -50,7 +62,109 @@ class Header extends StatelessWidget {
     return Container(
 
       height: 200,
-      child: Text("Header"),
+      child: Column(
+        children: [
+          //--- Componentes - Linea 01
+          Container(
+            margin: EdgeInsets.symmetric( horizontal: 20, vertical: 15 ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              textDirection: TextDirection.ltr,
+              children: [
+                Expanded(
+
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Icon(
+                        Icons.add_business
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                        "Kudos"
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.centerRight,
+                    child: Icon(
+                        Icons.shopping_cart_outlined
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //--- Componentes - Linea 02
+          Container(
+            margin: EdgeInsets.symmetric( horizontal: 20 ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    child: Text(
+                        "Favourites"
+                    ),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Container(
+                      width: 90,
+                      height: 30,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "VIEW ALL",
+                        style: TextStyle(
+                          fontSize: 8,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(20)
+                      ),
+                    ),
+                    alignment: Alignment.centerRight,
+                  ),
+                )
+              ],
+            ),
+              //--- Componentes - Linea 03
+          ),
+          ItemHeaderCustom(
+          cantidadEstrellas: 4,
+          title: "Washer Machine",
+          calificacion: "4.6",
+          imgAssets: "assets/Item01.png",
+          description: "Ready Wash Staininess",
+          )
+          /*SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      ItemHeaderCustom(
+                        cantidadEstrellas: 4,
+                        title: "Washer Machine",
+                        calificacion: "4.6",
+                        description: "Ready Wash Staininess",
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )*/
+        ],
+      ),
 
     );
 
@@ -84,7 +198,7 @@ class Body extends StatelessWidget {
                   descripcionItem: "Microwave Ove",
                   calificacionItem: "4.6"         ,
                   cantidadEstrellasItem: 4        ,
-                  imagen: "assets/Item03.png",
+                  imagen: "assets/Item01.png",
                 ),
                 ItemListCustom(
                   descripcionItem: "Wishk",
@@ -96,19 +210,19 @@ class Body extends StatelessWidget {
                   descripcionItem: "Speakers",
                   calificacionItem: "4.6"         ,
                   cantidadEstrellasItem: 4        ,
-                  imagen: "assets/Item01.png",
+                  imagen: "assets/Item03.png",
                 ),
                 ItemListCustom(
                   descripcionItem: "Air Conditioner",
                   calificacionItem: "5.0"         ,
                   cantidadEstrellasItem: 5        ,
-                  imagen: "assets/Item01.png",
+                  imagen: "assets/Item04.png",
                 ),
                 ItemListCustom(
                   descripcionItem: "Television",
                   calificacionItem: "5.0"         ,
                   cantidadEstrellasItem: 5        ,
-                  imagen: "assets/Item04.png",
+                  imagen: "assets/Item01.png",
                 ),
 
               ],
