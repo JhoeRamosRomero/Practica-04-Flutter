155e0bd712dfe43ce03ff6da0a8baf751d2ee378
Anthony Ramos Romero <anthonyramosdev@gmail.com>
2021-06-18 20:32:37 -0500

    Init -- Remote

--- lib/ui/favorite-ui.dart
+++ lib/ui/favorite-ui.dart
@@ -60,7 +60,11 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Container();
+    return Container(
+
+      child: Text("TESSSSSSSSSSSSSSSSss"),
+
+    );
 
   }
 
