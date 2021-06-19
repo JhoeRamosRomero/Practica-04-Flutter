e4fb00b80e6ed7e04a31f0897dc9bfb8b070fef2
Anthony Ramos Romero <anthonyramosdev@gmail.com>
2021-06-18 19:58:32 -0500

    Firs Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:practica04/ui/favorite-ui.dart';
+
+void main() => runApp( MyApp() );
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      title: "SponsorMe",
+      initialRoute: "favorite",
+      routes: {
+        'favorite': (_) => FavoriteUI(),
+      },
+
+    );
+
+  }
+
+
+}
---
+++ lib/ui/favorite-ui.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FavoriteUI extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: UI(),
+    );
+
+  }
+
+}
+
+
+class UI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Stack(
+      children: [
+        SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+
+              children: [
+
+                Header(),
+                Body()
+
+              ],
+
+            ),
+          ),
+        ),
+    ]
+
+    );
+
+  }
+
+}
+
+class Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+
+      height: 200,
+
+    );
+
+  }
+
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Container();
+
+  }
+
+}
