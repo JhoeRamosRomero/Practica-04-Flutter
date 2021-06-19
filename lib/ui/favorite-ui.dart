41ad2888aef39039d5e33c6d2a3d5736ffc57162
Anthony Ramos Romero <anthonyramosdev@gmail.com>
2021-06-18 23:23:50 -0500

    1. Configuración de Assets 2. Creación De Widget's para los iconos y lista 3. Mejora en la apariencia de la applicación
    Funcionalidad adicional para mostrar la cantidad de estrellas segun parametro, por ahora solo funciona con valores enteros

--- lib/ui/favorite-ui.dart
+++ lib/ui/favorite-ui.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:practica04/widget/item-list-custom.dart';
 
 class FavoriteUI extends StatelessWidget{
   @override
@@ -49,6 +50,7 @@ class Header extends StatelessWidget {
     return Container(
 
       height: 200,
+      child: Text("Header"),
 
     );
 
@@ -62,7 +64,61 @@ class Body extends StatelessWidget {
 
     return Container(
 
-      child: Text("TESSSSSSSSSSSSSSSSss"),
+      child: Column(
+        children: [
+
+          //--- Titulo
+
+          Container(
+            child: Text("ALL"),
+          ),
+
+          //--- ListView
+          SingleChildScrollView(
+            //--- Componente Detail
+            child: Column(
+
+              children: [
+
+                ItemListCustom(
+                  descripcionItem: "Microwave Ove",
+                  calificacionItem: "4.6"         ,
+                  cantidadEstrellasItem: 4        ,
+                  imagen: "assets/Item03.png",
+                ),
+                ItemListCustom(
+                  descripcionItem: "Wishk",
+                  calificacionItem: "3.3"         ,
+                  cantidadEstrellasItem: 3        ,
+                  imagen: "assets/Item02.png",
+                ),
+                ItemListCustom(
+                  descripcionItem: "Speakers",
+                  calificacionItem: "4.6"         ,
+                  cantidadEstrellasItem: 4        ,
+                  imagen: "assets/Item01.png",
+                ),
+                ItemListCustom(
+                  descripcionItem: "Air Conditioner",
+                  calificacionItem: "5.0"         ,
+                  cantidadEstrellasItem: 5        ,
+                  imagen: "assets/Item01.png",
+                ),
+                ItemListCustom(
+                  descripcionItem: "Television",
+                  calificacionItem: "5.0"         ,
+                  cantidadEstrellasItem: 5        ,
+                  imagen: "assets/Item04.png",
+                ),
+
+              ],
+
+            ),
+
+          )
+
+        ],
+      ),
 
     );
 
